9c02a2afc2543c15e699792557abff5b7b8f7bf6
SarthakSaraniya <127471359+SarthakSaraniya@users.noreply.github.com>
2024-02-05 13:04:16 +0530

    1

---
+++ Classwork/Date5 Feb/t1.py
@@ -0,0 +1,22 @@
+l = [1,2,"Sarthak",2.448,"Saraniya"]
+print(l)
+
+l.append(4.25)
+print(l)
+
+l1=l.copy()
+print(l1)
+
+print(l.count(1))
+
+l.extend([231,"Sahil","Ary"])
+print(l)
+
+l.insert(3,"Dhrmik")
+print(l)
+
+l.pop()
+print(l)
+
+l.remove(1)
+print(l)
