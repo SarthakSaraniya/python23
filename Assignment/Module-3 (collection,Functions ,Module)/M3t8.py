25e74d5f7f5f4c1f1e38a8c97ec601291b0a7de2
SarthakSaraniya <127471359+SarthakSaraniya@users.noreply.github.com>
2024-02-13 10:40:14 +0530

    1

---
+++ Assignment/Module-3 (collection,Functions ,Module)/M3t8.py
@@ -0,0 +1,9 @@
+# Given list of characters
+char_list = ['Sarthak', 'Saraniya', 'is', 'best', 'cricketer.']
+
+# Convert the list of characters to a string using the join() method
+result_string = ' '.join(char_list)
+
+# Print the resulting string
+print("Resulting string:", result_string)
+
