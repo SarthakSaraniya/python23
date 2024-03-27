f7dbfaa32e09c2f325c369090b568de88d66bf1b
SarthakSaraniya <127471359+SarthakSaraniya@users.noreply.github.com>
2024-03-28 01:33:02 +0530

    1

--- Djengo Project/myapp/models.py
+++ Djengo Project/myapp/models.py
@@ -9,3 +9,15 @@ class Contact(models.Model):
 
 	def __str__(self):
 		return self.name 
+
+class User(models.Model):
+	fname = models.CharField(max_length=100)
+	lname = models.CharField(max_length=100)
+	email = models.EmailField()
+	mobile = models.PositiveIntegerField()
+	address = models.TextField()
+	password = models.CharField(max_length=100)
+	profile_picture=models.ImageField(upload_to="profile_picture/",default="")
+
+	def __str__(self):
+		return self.fname+" "+self.lname
--- Djengo Project/myapp/views.py
+++ Djengo Project/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Contact
+from .models import Contact,User
+from django.conf import settings
+from django.core.mail import send_mail
+import random
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
@@ -20,7 +23,143 @@ def contact(request):
 		return render(request,'contact.html',{'contacts':contacts})
 
 def signup(request):
-	return render(request,'signup.html')
+	if request.method == "POST":
+		try :
+			User.objects.get(email=request.POST['email'])
+			msg = "Email Alredy Registered"
+			return render(request,'signup.html',{'msg': msg})
+		except :
+			if request.POST['password'] == request.POST['cpassword']:
+
+				User.objects.create(
+						fname=request.POST['fname'],
+						lname=request.POST['lname'],
+						email=request.POST['email'],
+						mobile=request.POST['mobile'],
+						address=request.POST['address'],
+						password=request.POST['password'],
+						profile_picture=request.POST['profile_picture']
+					)
+				msg = "User Sign Up Successfully"
+				return render(request,'signup.html',{'msg': msg})
+			else :
+				msg = "Password & Confirm Dose Not Matched"
+				return render(request,'signup.html',{'msg': msg})
+	else :	
+		return render(request,'signup.html')
 
 def login(request):
-	return render(request,'login.html')
+	if request.method == 'POST':
+		try :
+			user = User.objects.get(email=request.POST['email'])
+			if user.password == request.POST['password'] :
+				request.session['email'] = user.email
+				request.session['fname'] = user.fname
+				request.session['profile_picture'] = user.profile_picture.url
+				return render(request,'index.html')
+			else :
+				msg="Incorrect Password"
+				return render(request,'login.html',{'msg':msg})
+		except:
+			msg="Email not Registered"
+			return render(request,'login.html',{'msg':msg})
+
+	else:
+		return render(request,'login.html')
+
+def logout(request):
+	try :
+		del request.session['email']
+		del request.session['fname']
+		del request.session['profile_picture']
+		msg = "Logout Successfilly"
+		return render(request,'login.html',{'msg': msg})
+	except:
+		return render(request,'login.html')
+
+def change_password(request):
+	if request.method == 'POST':
+		user = User.objects.get(email=request.session['email'])
+		if user.password == request.POST['old_password']:
+			if request.POST['new_password']==request.POST['cnew_password']:
+				if user.password != request.POST['new_password'] :
+					user.password=request.POST['new_password']
+					user.save()
+					msg = "Password changed Successfilly. Please login Again."
+					del request.session['email']
+					del request.session['fname']
+					return render(request,'login.html',{'msg':msg})
+				else:
+					msg = "New Password Can't be your one of the Old Password"
+					return render(request,'change-password.html',{'msg':msg})
+			else :
+				msg = "New Password & Confirm Password Dose not Matched"
+				return render(request,'change-password.html',{'msg':msg})
+
+		else :
+			msg = "Old Password Dose not Matched"
+			return render(request,'change-password.html',{'msg':msg})
+	else:
+		return render(request,'change-password.html')
+
+def forgot_password(request):
+	if request.method=="POST" :
+		try :
+			user = User.objects.get(email=request.POST['email'])
+			otp = random.randint(1000,9999)
+			subject = 'OTP for Forgot Password'
+			message = 'Hello'+user.fname+',Ypur OTP for Forgot Password is '+str(otp)
+			email_from = settings.EMAIL_HOST_USER
+			recipient_list = [user.email, ]
+			send_mail( subject, message, email_from, recipient_list )
+			return render(request,'otp.html',{'email':user.email,'otp':otp})
+		except:
+			msg = "Email Not Registered"
+			return render(request,'forgot-password.html',{'msg':msg})	
+	else:
+		return render(request,'forgot-password.html')
+
+def verify_otp(request):
+	email=request.POST['email']
+	otp = int(request.POST['otp'])
+	uotp = int(request.POST['uotp'])
+
+	if otp==uotp:
+		return render(request,'new-password.html',{'email':email})
+	else:
+		msg = "Invalid OTP"
+		return render(request,'otp.html',{'email':email,'otp':otp,'msg':msg})
+
+def new_password(request):
+	email=request.POST['email']
+	np=request.POST['new_password']
+	cnp=request.POST['cnew_password']
+
+	if np==cnp:
+		user=User.objects.get(email=email)
+		user.password=np
+		user.save()
+		msg="Password Updated Successfilly"
+		return render(request,'login.html',{'msg':msg})
+
+	else:
+		msg="New Password & Confirm New Password dose not Matched"
+		return render(request,'new-password.html',{'email':email,'msg':msg})
+
+def profile(request):
+	user = User.objects.get(email=request.session['email'])
+	if request.method=="POST":
+		user.fname=request.POST['fname']
+		user.lname=request.POST['lname']
+		user.mobile=request.POST['mobile']
+		user.address=request.POST['address']
+		try:
+			user.profile_picture=request.FILES['profile_picture']
+		except:
+			pass
+		user.save()
+		msg="Profile Updated Successfilly"
+		request.session['profile_picture']=user.profile_picture.url
+		return render(request,'profile.html',{'user':user,'msg':msg})
+	else:
+		return render(request,'profile.html',{'user':user})
