c4168c8a94e2585251471dff4659f0b3c9ca067c
SarthakSaraniya <127471359+SarthakSaraniya@users.noreply.github.com>
2024-04-06 14:07:51 +0530

    1

---
+++ ArtistHub_Event_Management/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('rent-venue/',views.rent_venue,name='rent-venue'),
+    path('shows-events/',views.shows_events,name='shows-events'),
+    path('tickets/',views.tickets,name='tickets'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+]
