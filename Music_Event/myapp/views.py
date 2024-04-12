10c36845b125109b94fd5396aa7995c0b06ffda0
SarthakSaraniya <127471359+SarthakSaraniya@users.noreply.github.com>
2024-04-12 12:01:30 +0530

    1

--- Music_Event/myapp/views.py
+++ Music_Event/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import User
+import requests 
+import random
 
 # Create your views here.
 
@@ -97,3 +99,48 @@ def change_password(request):
 			return render(request,'change-password.html',{'msg':msg})
 	else:
 		return render(request,'change-password.html')
+
+def forgot_password(request):
+	if request.method=="POST":
+		try:
+			user=User.objects.get(mobile=request.POST['mobile'])
+			otp=str(random.randint(1000,9999))
+			mobile=str(request.POST['mobile'])
+			url = "https://www.fast2sms.com/dev/bulkV2"
+			querystring = {"authorization":"xWPDfw1r8KTc6gyznUQ9VYqv35FJpLmS0dBlho2AXuCaZHEiNeli6TxgOIbPwftLyR2Kzu4ZqajSoe9h","variables_values":otp,"route":"otp","numbers":mobile}
+			headers = {'cache-control': "no-cache"}
+			response = requests.request("GET", url, headers=headers, params=querystring)
+			print(response.text)
+			return render(request,'otp.html',{'otp':otp,'mobile':mobile})
+		except:
+			msg="mobile not registered"
+			return render(request,'forgot-password.html',{'msg':msg})
+	else:
+		return render(request,'forgot-password.html')
+
+def verify_otp(request):
+	otp=request.POST['otp']
+	uotp=request.POST['uotp']
+	mobile=request.POST['mobile']
+
+	if otp==uotp:
+		return render(request,'new-password.html',{'mobile':mobile})
+	else:
+		msg="invalid OTP"
+		return render(request,'otp.html',{'otp':otp,'mobile':mobile,'msg':msg})
+
+def new_password(request):
+	mobile=request.POST['mobile']
+	np=request.POST['new_password']
+	cnp=request.POST['cnew_password']
+
+	if np==cnp:
+		user=User.objects.get(mobile=mobile)
+		user.password=np
+		user.save()
+		msg="Password Updated Successfilly"
+		return render(request,'login.html',{'msg':msg})
+
+	else:
+		msg="New Password & Confirm New Password Dose not Matched"
+		return render(request,'new-password.html',{'msg':msg,'mobile':mobile})
