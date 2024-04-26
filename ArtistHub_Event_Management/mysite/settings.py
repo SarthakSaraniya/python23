6585f014ec0e3f45fe33b1aef98eaefa8067442c
SarthakSaraniya <127471359+SarthakSaraniya@users.noreply.github.com>
2024-04-26 20:15:56 +0530

    1

--- ArtistHub_Event_Management/mysite/settings.py
+++ ArtistHub_Event_Management/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qgj&u&f)+&2sdlfv2le-5lz=wq7kbd3m-3ani5f(!at_+)3m@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
